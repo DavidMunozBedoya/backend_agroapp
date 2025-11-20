3bf01825909024d3ffea673ed37a3b8a9ee937e0
davidMuñoz <munozd50@gmail.com>
2025-11-20 12:36:58 -0500

    actualizacion

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -5,14 +5,15 @@ export async function getUserController(req: Request, res: Response) {
     try {
         const users = await getUserDb();
         res.status(200).send({
-            status: "success",
+            status: "Success",
             data: users,
         })
 
     } catch (e) {
         res.status(500).send({
-            status: "error",
-            message: "Internal Server Error",
+            status: "Error",
+            message: `Internal Server Error: ${e}`,
         });
     }
 }
+
--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -1,10 +1,28 @@
 import { Conexion } from "../../config/dbConexion.ts";
 import bcrypt from "bcryptjs";
+/* import validateUser from "../helpers/validator.js" */
 
 export async function getUserDb() {
     try {
         const connection = await Conexion();
         const [rows] = await connection.query("SELECT * FROM users");
+        return rows;
+    } catch (e) {
+        console.log(e);
+        throw e; // Es buena práctica relanzar el error para que el controlador lo maneje
+    }
+}
+
+export async function CreateUserDb(name:string, email:string, password:string) {
+    try {
+        const connection = await Conexion();
+        // validaciones con validator
+        const errors = [];
+
+
+        const [rows] = await connection.query("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", 
+            [name, email, bcrypt.hashSync(password, 10)]
+        );
         return rows;
     } catch (e) {
         console.log(e);
